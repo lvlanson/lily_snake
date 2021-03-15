f1ad4f0098650014447d1c3ade40af4a00ebcc93
lvlanson <lvlanson@pop-os.localdomain>
2021-03-15 15:31:25 +0100

    Collision Detection, Richtungsanpassung der Schlange, Apfel eingezeichnet

--- helpers.py
+++ helpers.py
@@ -1,5 +1,6 @@
 import colors
 import pygame
+from random import randint
 
 
 # Zeichnet ein Gittermuster im Spielfeld. Das Fenster ist die Variable screen. Das Spielfeld wird durch top_left
@@ -10,3 +11,31 @@ def draw_grid(screen, top_left: tuple, field_size: tuple, step: int):
 
     for x in range(top_left[0] + step, top_left[0] + field_size[0], step):
         pygame.draw.line(screen, colors.BLACK, (x, top_left[1]), (x, top_left[1] + field_size[1]))
+
+
+def check_boundaries(top_left, field_size, snake_head, step_size):
+
+    print(snake_head)
+    #wenn schlange links drüber ist
+    if snake_head[0] <= top_left[0]:
+        return False
+    #wenn schlange rechts drüber ist
+    elif snake_head[0] >= top_left[0] + field_size[0] - step_size:
+        return False
+    #wenn schlange oben drüber ist
+    elif snake_head[1] <= top_left[1]:
+        return False
+    #wenn schlange untern drüber ist
+    elif snake_head[1] >= top_left[1] + field_size[1] - step_size:
+        return False
+    return True
+
+
+def get_apple_location(tep_left, field_size, step_size):
+    location = (0, 0)
+
+    return location
+
+
+if __name__ == "__main__":
+    print(get_apple_location((100, 100), (800, 600), 20))
--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # Import and initialize the pygame library
 import pygame
 import colors
-from helpers import draw_grid
+from helpers import draw_grid, check_boundaries
 
 pygame.init()
 
@@ -11,9 +11,6 @@ pygame.init()
 WIDTH  = 1000
 HEIGHT = 800
 screen = pygame.display.set_mode([WIDTH, HEIGHT])
-
-# Run until the user asks to quit
-running = True
 
 # Spielfeld
 #                   x ,  y
@@ -26,6 +23,10 @@ game_field      = pygame.Rect(TOP_LEFT, FIELD_SIZE)
 snake_pos       = (TOP_LEFT[0] + FIELD_SIZE[0] / 2, TOP_LEFT[1] + FIELD_SIZE[1] / 2)
 snake_head      = pygame.Rect(snake_pos, (STEP_SIZE, STEP_SIZE))
 
+# Apfel
+apple_pos       = (TOP_LEFT[0] + FIELD_SIZE[0] / 4, TOP_LEFT[1] + FIELD_SIZE[1] / 4)
+apple           = pygame.Rect(apple_pos, (STEP_SIZE, STEP_SIZE))
+
 # Timer
 clock = pygame.time.Clock()
 
@@ -35,52 +36,67 @@ GO_DOWN    = False
 GO_LEFT    = False
 GO_RIGHT   = True
 
-while running:
+# Run until the user asks to quit
+running = True
 
-    # Did the user click the window close button?
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                GO_UP       = True
-                GO_DOWN     = False
-                GO_LEFT     = False
-                GO_RIGHT    = False
-            elif event.key == pygame.K_DOWN:
-                GO_UP       = False
-                GO_DOWN     = True
-                GO_LEFT     = False
-                GO_RIGHT    = False
-            elif event.key == pygame.K_LEFT:
-                GO_UP       = False
-                GO_DOWN     = False
-                GO_LEFT     = True
-                GO_RIGHT    = False
-            elif event.key == pygame.K_RIGHT:
-                GO_UP       = False
-                GO_DOWN     = False
-                GO_LEFT     = False
-                GO_RIGHT    = True
+# Schlange ist noch aktiv
+snake_active = True
+while running:
 
     ### VORBEREITUNG SPIELFELD ###
     # Fill the background with white
     screen.fill((255, 255, 255))
     pygame.draw.rect(screen, (0, 0, 0, 0), game_field, 5, border_radius=10)
-    draw_grid(screen, TOP_LEFT, FIELD_SIZE, STEP_SIZE)
+    #draw_grid(screen, TOP_LEFT, FIELD_SIZE, STEP_SIZE)
 
-    # Position der Schlange berechnen
-    if GO_UP:
-        snake_pos = (snake_pos[0], snake_pos[1] - STEP_SIZE)
-    elif GO_DOWN:
-        snake_pos = (snake_pos[0], snake_pos[1] + STEP_SIZE)
-    elif GO_LEFT:
-        snake_pos = (snake_pos[0] - STEP_SIZE, snake_pos[1])
-    elif GO_RIGHT:
-        snake_pos = (snake_pos[0] + STEP_SIZE, snake_pos[1])
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                if not GO_DOWN:
+                    GO_UP = True
+                    GO_DOWN = False
+                    GO_LEFT = False
+                    GO_RIGHT = False
+            elif event.key == pygame.K_DOWN:
+                if not GO_UP:
+                    GO_UP = False
+                    GO_DOWN = True
+                    GO_LEFT = False
+                    GO_RIGHT = False
+            elif event.key == pygame.K_LEFT:
+                if not GO_RIGHT:
+                    GO_UP = False
+                    GO_DOWN = False
+                    GO_LEFT = True
+                    GO_RIGHT = False
+            elif event.key == pygame.K_RIGHT:
+                if not GO_LEFT:
+                    GO_UP = False
+                    GO_DOWN = False
+                    GO_LEFT = False
+                    GO_RIGHT = True
 
-    snake_head = pygame.Rect(snake_pos, (STEP_SIZE, STEP_SIZE))
+    if snake_active:
+        # Did the user click the window close button?
+
+        # Position der Schlange berechnen
+        if check_boundaries(TOP_LEFT, FIELD_SIZE, snake_head, STEP_SIZE):
+            if GO_UP:
+                snake_pos = (snake_pos[0], snake_pos[1] - STEP_SIZE)
+            elif GO_DOWN:
+                snake_pos = (snake_pos[0], snake_pos[1] + STEP_SIZE)
+            elif GO_LEFT:
+                snake_pos = (snake_pos[0] - STEP_SIZE, snake_pos[1])
+            elif GO_RIGHT:
+                snake_pos = (snake_pos[0] + STEP_SIZE, snake_pos[1])
+        else:
+            print("ENDE")
+            snake_active = False
+        snake_head = pygame.Rect(snake_pos, (STEP_SIZE, STEP_SIZE))
     pygame.draw.rect(screen, colors.SNAKE_GREEN, snake_head)
+    pygame.draw.rect(screen, colors.RED, apple)
     # Flip the display
     pygame.display.flip()
     clock.tick(10)
